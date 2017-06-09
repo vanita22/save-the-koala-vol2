e4e25563a1a32f78ed42c32878e80974528249d2
vanita22 <vanitayrafita2004@gmail.com>
2017-06-09 12:11:03 -0300

    subiendo archivos

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function (){
+	$("#pop").popover({trigger: 'focus'});
+	$("#pop").popover({placement: 'focus'});
+	$("#pop").popover({delay: {show: 800, hide: 300}});
+	$("#pop").popover({placement:'bottom', trigger:'hover'});
+});
