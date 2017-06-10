4d8ddf075181d468789da89ec155ccea51b28d68
vanita22 <vanitayrafita2004@gmail.com>
2017-06-09 23:25:08 -0300

    subiendo los nuevos archivos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-$(function (){
-	$("#pop").popover({trigger: 'focus'});
-	$("#pop").popover({placement: 'focus'});
-	$("#pop").popover({delay: {show: 800, hide: 300}});
-	$("#pop").popover({placement:'bottom', trigger:'hover'});
+ $(document).ready(function(){
+  $('[data-toogle="tooltip"]').tooltip();
 });
+
